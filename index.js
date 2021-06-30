4f0857e0b5a27fde8fef8a3aa30c453f96f631f5
Sajal Gupta <guptasajal411@gmail.com>
2021-06-30 19:15:09 +0530

    Add logic functions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var previousNumber;
-var CurrentNumber;
+var currentNumber;
 var operationType;
 
 $(".number").click(function(){
@@ -10,9 +10,36 @@ $(".operation").click(function(){
     previousNumber = $(".calculatorDisplay").val();
     operationType = $(this).html();
     $(".calculatorDisplay").val("hjkljk");
-    $(".previousDisplay").val(previousNumber + " " + operationType);
+    $(".previousDisplay").val(previousNumber + operationType);
 })
 
 $(".equal").click(function(){
-
+    currentNumber = $(".calculatorDisplay").val();
+    $(".previousDisplay").val($(".previousDisplay").val() + currentNumber);
+    if(operationType == "+"){
+        addition(previousNumber, currentNumber);
+    }
+    if(operationType == "-"){
+        subtraction(previousNumber, currentNumber);
+    }
+    if(operationType == "*"){
+        multiplication(previousNumber, currentNumber);
+    }
+    if(operationType == "/"){
+        division(previousNumber, currentNumber);
+    }
 })
+
+
+function addition(previousNumber, currentNumber){
+    $(".calculatorDisplay").val(parseInt(previousNumber) + parseInt(currentNumber));
+}
+function subtraction(previousNumber, currentNumber){
+    $(".calculatorDisplay").val(parseInt(previousNumber) - parseInt(currentNumber));
+}
+function multiplication(previousNumber, currentNumber){
+    $(".calculatorDisplay").val(parseInt(previousNumber) * parseInt(currentNumber));
+}
+function division(previousNumber, currentNumber){
+    $(".calculatorDisplay").val(parseInt(previousNumber) / parseInt(currentNumber));
+}
