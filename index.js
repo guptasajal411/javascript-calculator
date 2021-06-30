cb5b9e40db20c6503c3f2f8e92691612c8b80209
Sajal Gupta <guptasajal411@gmail.com>
2021-06-30 18:49:10 +0530

    Add eventlisteners to JavaScript

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var previousNumber;
-varCurrentNumber
+var CurrentNumber;
+var operationType;
 
 $(".number").click(function(){
     $(".calculatorDisplay").val($(".calculatorDisplay").val() + $(this).html());
+})
+
+$(".operation").click(function(){
+    previousNumber = $(".calculatorDisplay").val();
+    operationType = $(this).html();
+    $(".calculatorDisplay").val("hjkljk");
+    $(".previousDisplay").val(previousNumber + " " + operationType);
+})
+
+$(".equal").click(function(){
+
 })
